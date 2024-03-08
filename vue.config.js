8be49a5d2c77b11506a2631509aa89da027ef806
Nikita <nikita.shkuratt@gmail.com>
2024-03-08 12:12:44 +0200

    del host cheking

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  disableHostCheck: true,
   allowedHosts: 'all',
   devServer: {
     allowedHosts: [
